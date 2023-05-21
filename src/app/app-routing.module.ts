f70c7ee6f82e219d97b6af4d13819d48b827431e
PrinceG77 <princemalere7@gmail.com>
2023-05-21 18:06:39 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "./view/homepage/homepage.component";
+import {AboutpageComponent} from "./view/aboutpage/aboutpage.component";
+import {EventspageComponent} from "./view/eventspage/eventspage.component";
+import {ServicespageComponent} from "./view/servicespage/servicespage.component";
+import {NewspageComponent} from "./view/newspage/newspage.component";
+import {ContactpageComponent} from "./view/contactpage/contactpage.component";
+import {DonspageComponent} from "./view/donspage/donspage.component";
+import {EducationSpecifiqueComponent} from "./view/education-specifique/education-specifique.component";
+import {SuiviParamedicalComponent} from "./view/suivi-paramedical/suivi-paramedical.component";
+import {EquipePolyvalenteComponent} from "./view/equipe-polyvalente/equipe-polyvalente.component";
+import {BeneficiairesComponent} from "./view/beneficiaires/beneficiaires.component";
+import {
+  SoutienAccompagnementPsychiqueComponent
+} from "./view/soutien-accompagnement-psychique/soutien-accompagnement-psychique.component";
+import {PresentationComponent} from "./view/presentation/presentation.component";
+import {MissionComponent} from "./view/mission/mission.component";
+import {HistoireComponent} from "./view/histoire/histoire.component";
+import {PartenairesComponent} from "./view/partenaires/partenaires.component";
+import {ServicesAdministratifsComponent} from "./view/services-administratifs/services-administratifs.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : HomepageComponent},
+  {path : 'accueil', component : HomepageComponent},
+  {path : 'Apropos', component : AboutpageComponent},
+  {path : 'presentation', component : PresentationComponent},
+  {path : 'objectifs', component : MissionComponent},
+  {path : 'histoire', component : HistoireComponent},
+  {path : 'partenaires', component : PartenairesComponent},
+  {path : 'services-administratifs', component : ServicesAdministratifsComponent},
+  {path : 'evenements', component : EventspageComponent},
+  {path : 'services', component : ServicespageComponent},
+  {path : 'education-specifique', component : EducationSpecifiqueComponent},
+  {path : 'suivi-paramedical', component : SuiviParamedicalComponent},
+  {path : 'soutient-accompagnement', component : SoutienAccompagnementPsychiqueComponent},
+  {path : 'equipe-polyvalente', component : EquipePolyvalenteComponent},
+  {path : 'beneficiaires', component : BeneficiairesComponent},
+  {path : 'actualites', component : NewspageComponent},
+  {path : 'contact', component : ContactpageComponent},
+  {path : 'dons', component : DonspageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,56 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './view/menu/menu.component';
+import { FooterComponent } from './view/footer/footer.component';
+import { HomepageComponent } from './view/homepage/homepage.component';
+import { AboutpageComponent } from './view/aboutpage/aboutpage.component';
+import { ServicespageComponent } from './view/servicespage/servicespage.component';
+import { NewspageComponent } from './view/newspage/newspage.component';
+import { EventspageComponent } from './view/eventspage/eventspage.component';
+import { ContactpageComponent } from './view/contactpage/contactpage.component';
+import { DonspageComponent } from './view/donspage/donspage.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { EducationSpecifiqueComponent } from './view/education-specifique/education-specifique.component';
+import { SuiviParamedicalComponent } from './view/suivi-paramedical/suivi-paramedical.component';
+import { SoutienAccompagnementPsychiqueComponent } from './view/soutien-accompagnement-psychique/soutien-accompagnement-psychique.component';
+import { BeneficiairesComponent } from './view/beneficiaires/beneficiaires.component';
+import { EquipePolyvalenteComponent } from './view/equipe-polyvalente/equipe-polyvalente.component';
+import { PresentationComponent } from './view/presentation/presentation.component';
+import { MissionComponent } from './view/mission/mission.component';
+import { HistoireComponent } from './view/histoire/histoire.component';
+import { PartenairesComponent } from './view/partenaires/partenaires.component';
+import { ServicesAdministratifsComponent } from './view/services-administratifs/services-administratifs.component';
+import { FooterPageComponent } from './view/footer-page/footer-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomepageComponent,
+    AboutpageComponent,
+    ServicespageComponent,
+    NewspageComponent,
+    EventspageComponent,
+    ContactpageComponent,
+    DonspageComponent,
+    EducationSpecifiqueComponent,
+    SuiviParamedicalComponent,
+    SoutienAccompagnementPsychiqueComponent,
+    BeneficiairesComponent,
+    EquipePolyvalenteComponent,
+    PresentationComponent,
+    MissionComponent,
+    HistoireComponent,
+    PartenairesComponent,
+    ServicesAdministratifsComponent,
+    FooterPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
