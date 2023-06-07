9bf156a19d3ad2f67168934d2ada74fd72eefa68
PrinceG77 <princemalere7@gmail.com>
2023-06-07 12:15:51 +0100

    First commit

--- src/app/controller/model/formulaire-inscription.model.ts
+++ src/app/controller/model/formulaire-inscription.model.ts
@@ -1,12 +1,13 @@
 export class FormulaireInscription {
   id: number;
   nom: string;
-  dateNaissance: string; // Utiliser le type 'string' au lieu de 'Date'
+  dateNaissance: string; // Utiliser le type 'string' pour correspondre au modèle du backend
   adresse: string;
   contact: string;
   typeHandicap: string;
   besoins: string;
-  dateSoumission: string; // Utiliser le type 'string' au lieu de 'Date'
+  dateSoumission: string; // Utiliser le type 'string' pour correspondre au modèle du backend
   isProcessed: boolean;
   isValidated: boolean;
 }
+
--- src/app/view/formulaire-inscription/formulaire-inscription.component.ts
+++ src/app/view/formulaire-inscription/formulaire-inscription.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormulaireInscriptionService} from "../../controller/services/formulaireinscription.service";
 import {FormulaireInscription} from "../../controller/model/formulaire-inscription.model";
+import {format, isValid, parse, parseISO, set} from 'date-fns';
 
 @Component({
   selector: 'app-formulaire-inscription',
@@ -8,7 +9,7 @@ import {FormulaireInscription} from "../../controller/model/formulaire-inscripti
   styleUrls: ['./formulaire-inscription.component.css']
 })
 export class FormulaireInscriptionComponent implements OnInit {
-  public formInscription : FormulaireInscription = {
+  public formInscription: FormulaireInscription = {
     id: null,
     nom: '',
     dateNaissance: null,
@@ -16,7 +17,7 @@ export class FormulaireInscriptionComponent implements OnInit {
     contact: '',
     typeHandicap: '',
     besoins: '',
-    dateSoumission : null,
+    dateSoumission: null,
     isProcessed: false,
     isValidated: false
   };
@@ -28,12 +29,38 @@ export class FormulaireInscriptionComponent implements OnInit {
   soumettreFormulaire() {
 
 
+    const dateNaissance = parse(this.formInscription.dateNaissance, 'yyyy-MM-dd', new Date());
+    this.formInscription.dateNaissance = format(dateNaissance, 'dd/MM/yyyy');
 
+
+    // Convertir la date de soumission en objet Date
+    const dateSoumission = new Date();
+    this.formInscription.dateSoumission = format(dateSoumission, 'dd/MM/yyyy HH:mm:ss');
+
+
+
+    console.log(this.formInscription.dateNaissance);
+    console.log(this.formInscription.dateNaissance);
     this.formulaireInscriptionService.soumettreFormulaire(this.formInscription).subscribe(
-      () => {
-        alert("Formulaire soumis avec succès !!");
+      (response: string) => {
+        alert(response); // Afficher le message de succès
+
+        // Réinitialiser le formulaire après soumission
+        this.formInscription = {
+          id: null,
+          nom: '',
+          dateNaissance: null,
+          adresse: '',
+          contact: '',
+          typeHandicap: '',
+          besoins: '',
+          dateSoumission: null,
+          isProcessed: false,
+          isValidated: false
+        };
       },
-      error => {
+      (error) => {
+        console.log(error); // Afficher les détails de l'erreur dans la console
         alert("Impossible de soumettre le formulaire");
       }
     );
